5aebb3ac0e94660e6e8873cdf94b3321c7294178
emascapa <emascapa@gmail.com>
2022-07-15 17:04:59 +0200

    fix namespacein routeserviceprovider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -14,7 +14,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    protected $namespace = 'App\Http\Controllers\Admin';
+    protected $namespace = 'App\Http\Controllers';
 
     /**
      * The path to the "home" route for your application.
