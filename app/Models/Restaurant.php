8c1f4844838c71d9948d5202fab011628979df12
FedericoCapitani <federico.capitani@outlook.it>
2022-07-15 10:43:03 +0200

    change restaurant user relations

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Restaurant extends Model
 {
-    public function user(): HasOne
+    public function user(): BelongsTo
     {
-        return $this->hasOne(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function categories(): BelongsToMany
