ad9e7999875dc9f5bd102b5cdcc94f4b873ae3d2
MyUsername <romamargine@gmail.com>
2022-07-18 18:04:24 +0200

    Add fix admin/dishes/show img path

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
     <h1>{{$dish->name}}</h1>
-    <img src="{{$dish->cover_image}}" alt="{{$dish->name}}">
+    <img src='{{ substr($dish->cover_image, 0, 4) == "http" ? $dish->cover_image  :  asset("storage/" . $dish->cover_image)}}' alt="{{$dish->name}}">
     <p>Ingredients: {{$dish->ingredients}}</p>
     <p>desciption: {{$dish->description}}</p>
     <h5>Price: {{$dish->price}}</h5>
