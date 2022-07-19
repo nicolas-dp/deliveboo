52f457c6bde662595f3409394c7fd07c6b6a351e
emascapa <emascapa@gmail.com>
2022-07-19 11:56:41 +0200

    fix api.php

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 //api ristoranti -> ti restituisce tutti i ristoranti
-Route::get('restaurants', 'API\RestaurantController@index');
+Route::get('restaurants', 'Api\RestaurantController@index');
 
 //api ristorante -> ti restituisce il singolo ristorante
-//Route::get('restaurants/{restaurant:slug}', 'API\RestaurantController@index');
+Route::get('restaurants/{restaurant:slug}', 'Api\RestaurantController@show');
 
 //api categorie -> ti restituisce le categorie
-Route::get('categories', 'API\CategoryController@index');
+Route::get('categories', 'Api\CategoryController@index');
 
 //api piatti
-Route::get('dishes', 'API\DishController@index');
+Route::get('dishes', 'Api\DishController@index');
