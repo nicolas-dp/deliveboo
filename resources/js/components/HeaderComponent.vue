d3aa21bfb6f5ba6b26b9da378f6ce677c72bdaf3
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-07-19 16:52:45 +0200

    Add HeaderComponent

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="header">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="/"
+                    ><img
+                        height="70"
+                        src="/../img/logo-deliveroo.png"
+                        alt="logo deliveroo"
+                /></a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    class="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <router-link class="nav-link" :to="{ name: 'home' }"
+                                >Home</router-link
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <router-link
+                                class="nav-link"
+                                :to="{ name: 'about' }"
+                                >About</router-link
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <router-link
+                                class="nav-link"
+                                :to="{ name: 'restaurants' }"
+                                >Restaurants</router-link
+                            >
+                        </li>
+                    </ul>
+
+                    <div class="my-2 my-lg-0">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/admin">Accedi</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent",
+};
+</script>
+
+<style  lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.navbar {
+    background-color: #ffa700;
+
+    a {
+        color: white;
+        font-weight: bold;
+    }
+}
+</style>
