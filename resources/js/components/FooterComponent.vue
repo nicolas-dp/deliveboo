5f04ca8c5508f93d8aa82fd1c304df88e5e53fb8
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-07-19 16:53:00 +0200

    Add FooterComponent

---
+++ resources/js/components/FooterComponent.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="footer">
+        <div class="container">
+            <div class="row row-cols-3 row-cols-md-2 row-cols-lg-6 gap-3">
+                <div class="col list_style">
+                    <h3>Scopri Deliveroo</h3>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Investitori</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Chi siamo</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Ristoranti</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Altro</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Pressroom</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Il nostro blog</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Programmazione</a
+                            >
+                        </li>
+                         <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Design</a
+                            >
+                        </li>
+                         <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Lavora con noi</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Diventa nostro partner</a
+                            >
+                        </li>
+                    </ul>
+                </div>
+                <div class="col list_style">
+                    <h3>Note legali</h3>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Termini & Condizioni</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Informativa sulla privacy</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Cookies</a
+                            >
+                        </li>
+                    </ul>
+                </div>
+                <div class="col list_style">
+                    <h3>Scopri Deliveroo</h3>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Investitori</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Investitori</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Investitori</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Investitori</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Investitori</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Investitori</a
+                            >
+                        </li>
+                        <li>
+                            <a href="https://corporate.deliveroo.co.uk"
+                                >Investitori</a
+                            >
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FooterComponent",
+};
+</script>
+
+<style lang="scss">
+.footer {
+    background-image: url("/../img/bg-footer.webp");
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.list_style {
+    background-color: rgba(78, 76, 76, 0.445);
+    border-radius: 1rem;
+}
+</style>
