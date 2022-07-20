8a887949ddaa52e81a8895cf56abc60a2a9aa572
FedericoCapitani <federico.capitani@outlook.it>
2022-07-20 18:46:25 +0200

    fix conflict public js files

--- public/js/admin.js
+++ public/js/admin.js
@@ -53491,7 +53491,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\laravel\Project\deliveboo\resources\js\admin.js */"./resources/js/admin.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\Final-Project\deliveboo\resources\js\admin.js */"./resources/js/admin.js");
 
 
 /***/ })
