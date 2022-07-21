b087d24857ebbff83ce3f341d6d9b9d5c5fb65b2
MyUsername <romamargine@gmail.com>
2022-07-21 15:58:04 +0200

    Add validation client side - password register

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,13 +4,12 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
-import router from './router';
-import App from './views/App.vue';
-
+import router from "./router";
+import App from "./views/App.vue";
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -19,7 +18,27 @@ import App from './views/App.vue';
  */
 
 const app = new Vue({
-    el: '#root',
-    render: h => h(App),
-    router
+    el: "#root",
+    render: (h) => h(App),
+    router,
+});
+
+const app2 = new Vue({
+    el: "#app",
+    data() {
+        return {
+            password: "",
+            password_confirm: "",
+            password_check: 0,
+        };
+    },
+    methods: {
+        checkPassword() {
+            if (this.password == this.password_confirm && this.password.length >= 8) {
+                this.password_check = 1;
+            } else {
+                this.password_check = 2;
+            }
+        },
+    },
 });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,12 +85,10 @@
 
     </nav>
 
-    <div id="app">
         <main class="py-4">
             @yield('content')
         </main>
         
-    </div>
 </body>
 
 </html>
