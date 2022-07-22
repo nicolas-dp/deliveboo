97a926e24df070f2a7fdd09dd14126f5a8e53954
emascapa <emascapa@gmail.com>
2022-07-22 19:54:54 +0200

    separate app.js and front.js / fix bug in img on home view

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -11,20 +11,20 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/front.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/front.css') }}" rel="stylesheet">
 </head>
 
 <body>
 
-
-    <div id="app">
+    {{-- levato id="app" --}}
+    <div id="root">
         <main>
             @yield('content')
         </main>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/admin.js', 'public/js')
+    .js('resources/js/front.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/admin.scss', 'public/css')
+    .sass('resources/sass/front.scss', 'public/css')
     .copyDirectory('resources/img', 'public/img');
         
 
