12a7b7172e65751145e8819dad7900436ba0a036
emascapa <emascapa@gmail.com>
2022-07-22 16:06:52 +0200

    Add possibility to add/remove items inside the cart

--- resources/js/Pages/Restaurant.vue
+++ resources/js/Pages/Restaurant.vue
@@ -137,20 +137,43 @@
               {{ dishElement.name }}: {{ dishElement.amount }} x
               {{ dishElement.price }}€ =
               {{ dishElement.amount * dishElement.price }} €
-              <span class="btn btn-danger btn-sm" @click="removeItem(i)">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="white"
-                  class="bi bi-trash3-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"
-                  />
-                </svg>
-              </span>
+
+              <div class="cartActions">
+                <div class="btn-group" role="group" aria-label="Basic example">
+                  <button
+                    type="button"
+                    class="btn btn-primary btn-sm"
+                    @click="subtractOne(dishElement, i)"
+                  >
+                    -
+                  </button>
+                  <span class="bg-light px-2 border border-1 d-flex align-items-center">
+                    <!-- :class="`dish-${dish.id}`" -->
+                    {{ dishElement.amount }}
+                  </span>
+                  <button
+                    type="button"
+                    class="btn btn-primary btn-sm"
+                    @click="addOne(dishElement)"
+                  >
+                    +
+                  </button>
+                </div>
+                <span class="btn btn-danger btn-sm" @click="removeItem(i)">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="white"
+                    class="bi bi-trash3-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"
+                    />
+                  </svg>
+                </span>
+              </div>
             </li>
           </ul>
 
@@ -203,7 +226,7 @@ export default {
           } else {
             this.restaurant = response.data;
 
-/*             this.restaurant.dishes.forEach((dish) => {
+            /*             this.restaurant.dishes.forEach((dish) => {
               dish["amount_html"] = 1;
             }); */
             //this.postResponse = response.data;
@@ -216,21 +239,35 @@ export default {
         });
     },
 
-    /*     addDish(id) {
-      let amount = document.querySelector(`dish-${id}`).value;
+    addOne(obj) {
+      //this.counter = this.counter + 1;
+      obj.amount = obj.amount + 1;
+      this.myCart.makeTotal();
+    },
 
+    subtractOne(obj, index) {
 
-    } */
+      //console.log(obj.amount);
+      //console.log(obj.amount);
+
+      if (obj.amount > 0) {
+        obj.amount = obj.amount - 1;
+        if (obj.amount == 0) {
+          this.removeItem(index)
+          this.myCart.makeTotal();
+        } else {
+          this.myCart.makeTotal();
+        }
+      }
+    },
+
     removeItem(index) {
       this.myCart.list_dishes.splice(index, 1);
 
       this.myCart.makeTotal();
     },
 
-    setCartCookie() {
-      
-    }
-
+    setCartCookie() {},
   },
 
   mounted() {
