5db0435f2699d5a29aea554408f4f71ffe468189
FedericoCapitani <federico.capitani@outlook.it>
2022-07-25 16:19:00 +0200

    add info to restaurant show page

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -4,17 +4,24 @@
 
 @include('partials.session_message')
 
-<div class="container text-center pt-4 pb-5">
+<div class="container pt-4 pb-5">
 
-    <h1>RISTORANTEEE</h1>
+    <h1 class="tetx-center">{{$restaurant->name}}</h1>
 
-    <p>{{$restaurant->name}}</p>
+    <img class="img-fluid" src='{{ substr($restaurant->cover_image, 0, 4) == "http" ? $dish->cover_image  :  asset("storage/" . $restaurant->cover_image)}}' alt="immagine copertina ristorante">
+    <p>Indirizzo: {{$restaurant->address}}</p>
+    <p>Numero di telefono: {{$restaurant->phone_number}}</p>
+    <p>Descrizione: </p>
     <p>{{$restaurant->description}}</p>
-    <p>{{$restaurant->address}}</p>
-    <p>{{$restaurant->delivery_cost}}</p>
-    <img src="{{$restaurant->cover_image}}" alt="">
+    <p>Orario di apertura: {{$restaurant->opening_hours}}</p>
+    <p>Orario di chiusura: {{$restaurant->closing_hours}}</p>
+    <p>Prezzo consegne: {{$restaurant->delivery_cost}}€</p>
+    <p>Partita iva: {{$restaurant->PIVA}}</p>
+    
 
+    <h4>Modifica le informazioni del tuo ristorante</h4>
     <a class="btn btn-success text-light" href="{{route('admin.restaurants.edit', $restaurant->slug)}}">Edit</a>
+    
 
 </div>
 @endsection
