77d244f8ba9eef7656072cf4dabb60e5e67b9336
MyUsername <romamargine@gmail.com>
2022-07-25 17:17:37 +0200

    Add New Layout For Verify Form and Fix class name for register form

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
+<div id="app">
+    <div class="check-form pt-5">
+        <div class="container container-form pt-5">
+            <div class="d-flex justify-content-center h-100 pt-5">
+                <div class="card card-form-xs">
+                    <div class="card-header">
+                        <div class="arrow-back">
+                            <a href="/"><strong>
+                                    <h3 class="pt-2">&LeftArrow;</h3>
+                                </strong>
+                            </a>
+                        </div>
+                        <h3>{{ __('Reset Password') }}</h3>
+                    </div>
+                    <div class="card-body">
+                        @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
+                        @endif
+                        <form method="POST" action="{{ route('password.email') }}">
+                            @csrf
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="input-group ">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text pb-3"><i class="fas fa-at"></i></span>
+                                </div>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="e-mail">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
-                            </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            </div>
+
+
+                            <div class="mt-3 mb-2 d-flex justify-content-center">
+                                <button type="submit" class="btn btn-primary float-right login_btn login_btn_plus">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
+                        </form>
+                    </div>
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-center links">
+                            Back to login<a class="" href="{{ route('login') }}">{{ __('login') }}</a>
                         </div>
-                    </form>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
