0afea8d8fb77a3f571638afa63f643462d4aa4ac
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-07-26 15:43:29 +0200

    Add strcture for get api braintree

--- resources/js/Pages/Checkout.vue
+++ resources/js/Pages/Checkout.vue
@@ -1,43 +1,74 @@
 <template>
-  <div class="checkout">
-    <div v-if="true">
-      <h1>Il tuo carrello</h1>
+    <div class="checkout">
+        <div v-if="true">
+            <h1>Il tuo carrello</h1>
 
-      <p>eccolo</p>
-      <h1>Il tuo carrello</h1>
+            <p>eccolo</p>
+            <h1>Il tuo carrello</h1>
 
-      <p>eccolo</p>
-      <h1>Il tuo carrello</h1>
+            <p>eccolo</p>
+            <h1>Il tuo carrello</h1>
 
-      <p>eccolo</p>
-      <h1>Il tuo carrello</h1>
+            <p>eccolo</p>
+            <h1>Il tuo carrello</h1>
 
-      <p>eccolo</p>
-      <h1>Il tuo carrello</h1>
+            <p>eccolo</p>
+            <h1>Il tuo carrello</h1>
 
-      <p>eccolo</p>
-      <h1>Il tuo carrello</h1>
+            <p>eccolo</p>
+            <h1>Il tuo carrello</h1>
 
-      <p>eccolo</p>
-      <h1>Il tuo carrello</h1>
+            <p>eccolo</p>
+            <h1>Il tuo carrello</h1>
 
-      <p>eccolo</p>
-      <h1>Il tuo carrello</h1>
+            <p>eccolo</p>
+            <h1>Il tuo carrello</h1>
 
-      <p>eccolo</p>
+            <p>eccolo</p>
+            <div class="text-2xl">
+                Stai acquistando il prodotto con id: {{ $product }}
+            </div>
+            <PaymentComponent
+                ref="paymentRef"
+                :authorization="tokenApi"
+                @loading="handleLoading"
+                @onSuccess="paymentOnSuccess"
+                @onError="paymentOnError"
+            />
+
+            
+        </div>
+        <div v-else></div>
     </div>
-    <div v-else></div>
-  </div>
 </template>
 
 <script>
+import PaymentComponent from "../components/PaymentComponent.vue";
+import axios from "axios";
+
 export default {
-  name: "Checkout",
+    components: { PaymentComponent },
+    name: "Checkout",
+    data() {
+        return {
+            tokenApi: "",
+            errors: [],
+        };
+    },
+
+    async created() {
+        try {
+            const response = await axios.get("/api/generate");
+            this.tokenApi = response.data.token;
+        } catch (e) {
+            this.errors.push(e);
+        }
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .checkout {
-  margin-top: 7rem;
+    margin-top: 7rem;
 }
 </style>
--- resources/js/front.js
+++ resources/js/front.js
@@ -20,7 +20,6 @@
  import "aos/dist/aos.css";
  AOS.init();
  
- 
  /**
   * Next, we will create a fresh Vue application instance and attach it to
   * the page. Then, you may begin adding components to this application
